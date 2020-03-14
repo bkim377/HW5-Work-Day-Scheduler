1420c2a2aedcf2e9035c3e7a347178f5009fae50
Brandon Kim <bkim377@gatech.edu>
2020-03-13 20:03:11 -0400

    Checkpoint 4 - got the colored rows to work with the current time

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ for (var i = 0; i < hourArray.length; i++) {
     colwrap1.addClass("col-lg-2 time-block hour row");
     // A div for the second column (where you enter text)
     var colwrap2 = $("<textarea></textarea>");
-    colwrap2.attr("id", "textbox" + (i+1));
+    colwrap2.attr("id", "textbox"+(i+1));
     colwrap2.addClass("col-lg-9 row past");
     // A div for the third column (save button)
     var colwrap3 = $("<button type='submit'></button>");
@@ -33,38 +33,37 @@ for (var i = 0; i < hourArray.length; i++) {
     hourWrap.append(colwrap3);
 }
 
-// adds hour names and unique ID's for all timeblock rows
+// adds hour names for all timeblock rows
     var cls = $(".time-block");
-    var colColor = $(".col-lg-9");
-    idArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     for (n = 0; n < 3; n++) {
       cls[n].append(hourArray[n] + "am");
-    //   colColor.addClass("hour-"+idArray[n]);
     }
     for (n = 3; n < 9; n++) {
       cls[n].append(hourArray[n] + "pm");
-    //   colColor.addClass("hour-"+idArray[n]);
 }
 
 // Check each row to see if they are past, present, or future hours
+// and colorcode them accordingly
 // for loop 1: hours 9am-12pm
-for (var i = 0; i < 3; i++) {
-    if (hourArray[i] == moment().hour()) {
-        colColor.addClass("present").removeClass("past future");
+
+var colColor = $(".past");
+for (var i = 0; i < 4; i++) {
+    if (hourArray[i] === moment().hour()) {
+        $("#textbox"+(i+1)).addClass("present").removeClass("past future");
     } else if (hourArray[i] < moment().hour()) {
-        colwrap2.addClass("past").removeClass("present future");
+        $("#textbox"+(i+1)).addClass("past").removeClass("present future");
     } else if (hourArray[i] > moment().hour()){
-        colwrap2.addClass("future").removeClass("past present");
+        $("#textbox"+(i+1)).addClass("future").removeClass("past present");
     }
 }
 //for loop 2: hours 1pm-5pm
-for (var i = 3; i < 9; i++) {
-    if ((hourArray[i] + 12) == moment().hour()) {
-        colColor.addClass("present").removeClass("past future");
+for (var i = 4; i < 9; i++) {
+    if ((hourArray[i] + 12) === moment().hour()) {
+        $("#textbox"+(i+1)).addClass("present").removeClass("past future");
     } else if ((hourArray[i] + 12) < moment().hour()) {
-        colwrap2.addClass("past").removeClass("present future");
+        $("#textbox"+(i+1)).addClass("past").removeClass("present future");
     } else if ((hourArray[i] + 12) > moment().hour()){
-        colwrap2.addClass("future").removeClass("past present");
+        $("#textbox"+(i+1)).addClass("future").removeClass("past present");
     }
 }
 
